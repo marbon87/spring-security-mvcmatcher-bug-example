3106a6dee2883d77e2f21abc6d5a32cbd467ec89
Mark Bonnekessel <m.bonnekessel@lvm.de>
2023-02-21 12:02:27 +0100

    init

---
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -0,0 +1,21 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.ServletRegistrationBean;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.Bean;
+
+@SpringBootApplication
+public class DemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DemoApplication.class, args);
+    }
+
+    @Bean
+    public ServletRegistrationBean<DemoServlet> messageDispatcherServlet() {
+        DemoServlet servlet = new DemoServlet();
+        return new ServletRegistrationBean<>(servlet, "/demoservlet/*");
+    }
+}
---
+++ src/test/java/com/example/demo/WorkingTest.java
@@ -0,0 +1,47 @@
+package com.example.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.context.TestConfiguration;
+import org.springframework.boot.test.web.server.LocalServerPort;
+import org.springframework.boot.web.client.RestTemplateBuilder;
+import org.springframework.context.annotation.Bean;
+import org.springframework.http.HttpStatusCode;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.web.client.RestTemplate;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.springframework.security.web.util.matcher.AntPathRequestMatcher.antMatcher;
+
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+class WorkingTest {
+    @TestConfiguration
+    @EnableWebSecurity
+    static class SecurityConfig {
+        @Bean
+        public SecurityFilterChain configure(HttpSecurity http) throws Exception {
+            return http.authorizeHttpRequests(customizer -> customizer
+                    .requestMatchers(antMatcher("/demoservlet/**")).permitAll()
+            ).build();
+        }
+    }
+
+    @Autowired
+    RestTemplateBuilder restTemplateBuilder;
+
+    @LocalServerPort
+    private int port;
+
+    @Test
+    void helloIsWorking() {
+        RestTemplate restTemplate = restTemplateBuilder.build();
+
+        String uri = "http://localhost:" + port + "/demoservlet/hello";
+        ResponseEntity<String> forEntity = restTemplate.getForEntity(uri, String.class);
+        assertThat(forEntity.getStatusCode()).isEqualTo(HttpStatusCode.valueOf(200));
+    }
+}
